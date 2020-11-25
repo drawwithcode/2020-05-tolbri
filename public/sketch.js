c40d4da8eed5da9a5c61f4ee5fbfc1b29365e5fc
tolbri <65823209+tolbri@users.noreply.github.com>
2020-11-25 01:29:23 +0100

    Drawing

--- public/sketch.js
+++ public/sketch.js
@@ -1,42 +1,106 @@
 let socket = io();
+let users = 1;
+let size = 50;
 
-function preload(){
+function preload() {
   // put preload code here
 }
 
 function setup() {
-  createCanvas(windowWidth,windowHeight)
+  createCanvas(windowWidth, windowHeight);
+  noFill();
+  rectMode(CENTER);
+  noCursor();
+  background("#E3F2FD");
+  strokeWeight(2);
+
   // put setup code here
 }
 
 function draw() {
   // put drawing code here
-  ellipse(mouseX, mouseY, 40);
 }
 
+function drawLine(data) {
+  stroke(data.c);
+  if(data.l == true) {
+    line(data.x, data.y, data.pX, data.pY);
+  } else {
+    line(data.x + windowWidth / 2, data.y, data.pX + windowWidth / 2, data.pY);
+  }
 
 
+}
+
+function resizeArea() {
+  stroke("#90CAF9");
+  square(windowWidth / 2 - windowWidth / 4, windowHeight / 2, size * users);
+}
 
 
 function mouseMoved() {
-  let message = {
-    x: mouseX,
-    y: mouseY
+
+  if (
+    mouseX > windowWidth / 2 - windowWidth / 4 - size / 2 * users &&
+    mouseX < windowWidth / 2 - windowWidth / 4 + size / 2 * users &&
+    mouseY > windowHeight / 2 - size / 2 * users &&
+    mouseY < windowHeight / 2 + size / 2 * users
+  ) {
+    let data = {
+      x: mouseX,
+      y: mouseY,
+      pX: pmouseX,
+      pY: pmouseY,
+      c: "#1565C0",
+      l: true
+    }
+    drawLine(data);
+
+    let message = {
+      x: mouseX,
+      y: mouseY,
+      pX: pmouseX,
+      pY: pmouseY,
+      c: "#00897B",
+      l: false
+    }
+    socket.emit("mouse", message);
+  } else {
+    let data = {
+      x: mouseX,
+      y: mouseY,
+      pX: pmouseX,
+      pY: pmouseY,
+      c: "#BBDEFB",
+      l: true
+    }
+    drawLine(data);
   }
-  socket.emit("mouse", message);
+
 }
 
-socket.on("connect", newConnection);
-
-function newConnection() {
-  console.log("Your ID: ", socket.id);
+function resetSpace(data) {
+  push();
+  rectMode(CORNER);
+  noStroke();
+  fill("#0D47A1");
+  let m = map(data, 0, 60, 0, windowWidth);
+  rect(0, 0, m, 5);
+  pop();
+  if (data >= 60) {
+        background("#E3F2FD");
+        resizeArea();
+  }
 }
 
+socket.on("reset", resetSpace);
+socket.on("mouseBroadcast", drawLine);
+socket.on("userCount", userCount);
 
-socket.on("mouseBroadcast", otherMouse);
-
-
-function otherMouse(data) {
-  fill("yello");
-  ellipse(data.x, data.y, 40);
+function userCount(data) {
+  if (users != data) {
+      users = data;
+      setTimeout(resizeArea,1000);
+  }
+  console.log("Connected Users: " + users);
 }
--- server.js
+++ server.js
@@ -12,14 +12,27 @@ let io = socket(server);
 app.use(express.static("public"));
 
 
+let time = 60;
+let userCount = 0;
 io.on("connection", newConnection);
-
 function newConnection(socket) {
-  console.log("new connection: " + socket.client.id);
-  socket.on("mouse", mouseMessage);
-
-  function mouseMessage(dataReceived) {
+  userCount = userCount + 1;
+  console.log("User Connected. User Count: " + userCount);
+  io.emit("userCount", userCount);
+  socket.on("mouse", function(dataReceived) {
     socket.broadcast.emit("mouseBroadcast", dataReceived);
-  }
-
+  });
+  socket.on("disconnect", function() {
+    userCount = userCount - 1;
+    console.log("User Disconnected. User Count: " + userCount);
+    io.emit("userCount", userCount);
+  });
 }
+
+var timer = setInterval(function(){
+  time = time + 1;
+  io.emit("reset", time);
+  if (time >= 60) {
+    time = 0;
+  }
+}, 1000)
